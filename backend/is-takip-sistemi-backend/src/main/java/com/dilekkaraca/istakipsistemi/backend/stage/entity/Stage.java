456d734174b0ba48ccf0f5c732b02cf012c683e2
Dilek Karaca <dilek.karaca@mf.karaelmas.edu.tr>
2026-05-09 12:55:15 +0300

    token testi

--- backend/is-takip-sistemi-backend/src/main/java/com/dilekkaraca/istakipsistemi/backend/stage/entity/Stage.java
+++ backend/is-takip-sistemi-backend/src/main/java/com/dilekkaraca/istakipsistemi/backend/stage/entity/Stage.java
@@ -7,7 +7,7 @@ import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-//m dnlakjdkjdfjfdsjd
+//m dnlakjdkjdfjfdsjddkfdkjkjdwjfdkjdsjd
 @Entity
 @Table(name = "stages")
 @Getter
