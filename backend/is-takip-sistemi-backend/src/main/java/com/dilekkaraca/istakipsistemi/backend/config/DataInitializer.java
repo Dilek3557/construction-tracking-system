9de69f0128249083069c8ae6b208745b66bbaaa1
Dilek Karaca <dilek.karaca@mf.karaelmas.edu.tr>
2026-05-12 22:52:31 +0300

    feat: docker compose frontend and fixes

---
+++ backend/is-takip-sistemi-backend/src/main/java/com/dilekkaraca/istakipsistemi/backend/config/DataInitializer.java
@@ -0,0 +1,34 @@
+package com.dilekkaraca.istakipsistemi.backend.config;
+
+import com.dilekkaraca.istakipsistemi.backend.user.entity.User;
+import com.dilekkaraca.istakipsistemi.backend.user.enums.UserRole;
+import com.dilekkaraca.istakipsistemi.backend.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+
+public class DataInitializer implements CommandLineRunner {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        long activeAdminCount = userRepository.countByRoleAndActiveTrue(UserRole.ADMIN);
+
+        if (activeAdminCount > 0) {
+            return;
+        }
+        User admin=User.builder()
+                .username("Mustafa")
+                .displayName("YÖNETİCİ")
+                .passwordHash(passwordEncoder.encode("473529"))
+                .role(UserRole.ADMIN)
+                .active(true)
+                .build();
+        userRepository.save(admin);
+    }
+}
